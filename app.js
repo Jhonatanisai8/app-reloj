14a4235076f31c909afdaeadf720d30fc449fb30
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-14 22:14:56 -0500

    funcionalidad js

---
+++ app.js
@@ -0,0 +1,45 @@
+const cronometro = document.getElementById("cronometro");
+const botonInicioPause = document.getElementById("boton-inicio-pausa");
+const botonReiniciar = document.getElementById("boton-reiniciar");
+
+let [horas, minutos, segundos] = [0, 0, 0];
+
+let intervaloTiempo;
+let estado = "pausado";
+
+function actualizarCronometro() {
+  segundos++;
+  if (segundos / 60 === 1) {
+    segundos = 0;
+    minutos++;
+    if (minutos / 60 === 1) {
+      minutos = 0;
+      horas++;
+    }
+  }
+
+  const segundosFormato = asignarFormato(segundos);
+  const minutosFormato = asignarFormato(minutos);
+  const horasFormato = asignarFormato(horas);
+  cronometro.innerText = `${horasFormato}:${minutosFormato}:${segundosFormato}`;
+}
+
+function asignarFormato(unidadTiempo) {
+  return unidadTiempo < 10 ? "0" + unidadTiempo : unidadTiempo;
+}
+
+botonInicioPause.addEventListener("click", () => {
+  if (estado === "pausado") {
+    intervaloTiempo = window.setInterval(actualizarCronometro, 1000);
+    botonInicioPause.innerHTML = "<i class='bi bi-pause-fill'></i>";
+    botonInicioPause.classList.remove("iniciar");
+    botonInicioPause.classList.add("pausar");
+    estado = "iniciado";
+  } else {
+    window.clearInterval(intervaloTiempo);
+    botonInicioPause.innerHTML = "<i class='bi bi-play-fill'></i>";
+    botonInicioPause.classList.remove("pausar");
+    botonInicioPause.classList.add("iniciar");
+    estado = "pausado";
+  }
+});
